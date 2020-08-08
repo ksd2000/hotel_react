0c7c1ece6fa3bec20eb24047a4494277cc7ff6fc
ksd2000 <ksd2000@yandex.ru>
2020-08-09 00:29:21 +0300

    Updates

---
+++ precache-manifest.826b0d42f5f7d5bc3a16b5076c6aff13.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df33822ec6ec76aa53473b6df67f795f",
+    "url": "/hotel_react/index.html"
+  },
+  {
+    "revision": "5a5e580d306cb4dcab7e",
+    "url": "/hotel_react/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "37128b77226c808cdd70",
+    "url": "/hotel_react/static/css/main.be17b6ca.chunk.css"
+  },
+  {
+    "revision": "5a5e580d306cb4dcab7e",
+    "url": "/hotel_react/static/js/2.c6d59b26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hotel_react/static/js/2.c6d59b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37128b77226c808cdd70",
+    "url": "/hotel_react/static/js/main.fbf1634a.chunk.js"
+  },
+  {
+    "revision": "5fb33fa02f7afe2d6009",
+    "url": "/hotel_react/static/js/runtime-main.922066db.js"
+  },
+  {
+    "revision": "c93ebeee87ede897ab024b39926676ae",
+    "url": "/hotel_react/static/media/Logo.c93ebeee.png"
+  },
+  {
+    "revision": "d6f4ecea85ee7b483a3bfff567bc6219",
+    "url": "/hotel_react/static/media/blue.d6f4ecea.png"
+  },
+  {
+    "revision": "72268bcc04c031dfce228ed6ddc65568",
+    "url": "/hotel_react/static/media/fot1.72268bcc.jpg"
+  },
+  {
+    "revision": "04c6eb5b69f1ef0ae8543832a622eadf",
+    "url": "/hotel_react/static/media/fot2.04c6eb5b.jpg"
+  },
+  {
+    "revision": "68c061ca74315b827a3284366e8cff91",
+    "url": "/hotel_react/static/media/fot3.68c061ca.jpg"
+  },
+  {
+    "revision": "9e5b324b3febb52b45585a711ff809b6",
+    "url": "/hotel_react/static/media/fot4.9e5b324b.png"
+  },
+  {
+    "revision": "d52443c7de25df5c99a69ee63c4eb141",
+    "url": "/hotel_react/static/media/fot5.d52443c7.png"
+  },
+  {
+    "revision": "a9a7e3cff500b3bf4e8be073dcb36bb1",
+    "url": "/hotel_react/static/media/fot6.a9a7e3cf.png"
+  },
+  {
+    "revision": "33675dc2e301a539b4545c3659fac580",
+    "url": "/hotel_react/static/media/fot7.33675dc2.png"
+  },
+  {
+    "revision": "26f2d610194fd5aca68465c87fd6dc30",
+    "url": "/hotel_react/static/media/home.26f2d610.png"
+  },
+  {
+    "revision": "0084558f920bc7aca97ee4f335b17894",
+    "url": "/hotel_react/static/media/man5.0084558f.png"
+  }
+]);
